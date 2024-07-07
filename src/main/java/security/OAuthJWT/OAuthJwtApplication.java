00e1e2bc28768fa889d91d001bd88688db44411c
veronees <jsg6854@naver.com>
2024-07-07 20:50:53 +0900

    first

---
+++ src/main/java/security/OAuthJWT/OAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package security.OAuthJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/security/OAuthJWT/OAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package security.OAuthJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
