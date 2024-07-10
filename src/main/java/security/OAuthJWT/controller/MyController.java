847b3c8fb98884951d6cdc1b135711843ded3488
veronees <jsg6854@naver.com>
2024-07-10 13:20:52 +0900

    yml2

--- src/main/java/security/OAuthJWT/controller/MyController.java
+++ src/main/java/security/OAuthJWT/controller/MyController.java
@@ -1,6 +1,5 @@
 package security.OAuthJWT.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import security.OAuthJWT.dto.UserDTO;
